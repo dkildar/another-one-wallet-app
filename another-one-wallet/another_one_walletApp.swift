84301bfcd782623997088cf3ac8fa23e3b087fd9
ildar.timerbaev <dkildar@gmail.com>
2024-04-09 12:15:58 +0600

    Initial Commit

---
+++ another-one-wallet/another_one_walletApp.swift
@@ -0,0 +1,20 @@
+//
+//  another_one_walletApp.swift
+//  another-one-wallet
+//
+//  Created by Ildar Timerbaev on 9.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct another_one_walletApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
